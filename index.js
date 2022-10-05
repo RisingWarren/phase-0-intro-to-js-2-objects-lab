90f5fb80802fab611b64861546135998e7ba8b33
Warren Santiago <wsantiago9623@gmail.com>
2022-10-04 22:54:42 -0400

    commit tree 25d858d7fc0beb010a684aa9efa1b91ecd4b5c78

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+const employee = { 
+    name:{},
+    StreetAddress:{},
+
+}
+
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updatedEmployee = {...employee};
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+     employee[key]=undefined;
+     return employee 
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value;
+    return employee 
+
+}
+
+function deleteFromEmployeeByKey(employee, key, value){
+    const updatedEmployee = {...employee};
+    updatedEmployee[key] = undefined;
+    return updatedEmployee;
+}
